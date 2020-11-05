bcbfdf2248e8f3bf65270ce83e60b3676757bf05
Briocodes <briocodes@gmail.com>
2020-11-05 17:17:52 +0100

    Error and Exception handling improved

--- src/studentdbms/Student.java
+++ src/studentdbms/Student.java
@@ -1,11 +1,12 @@
 package studentdbms;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Student {
     private String firstname;
     private String lastname;
-    private String gradeYear;
+    private int gradeYear;
     private String studentID;
     private String courses = null;
     private int tuitionBalance = 0;
@@ -23,9 +24,11 @@ public class Student {
         System.out.print("Enter student's lastname: ");
         this.lastname = scanner.nextLine();
 
-        System.out.print("\nACADEMIC GRADE LEVELS\n1 - Junior\n2 - Senior\n3 - Intermediate\n4 - Professional\nEnter " +
-                "student's grade level: ");
-        this.gradeYear = scanner.nextLine();
+        setGradeLevel();
+        while (gradeYear<1 || gradeYear>4){
+            setGradeLevel();
+        }
+
         setStudentID();
         optionsMenu();
     }
@@ -56,7 +59,8 @@ public class Student {
 
     //TODO 6 - View balance
     public void viewBalance(){
-        System.out.println("TUITION BALANCE: $" + tuitionBalance);
+        System.out.println("    TUITION BALANCE: $" + tuitionBalance);
+        System.out.println("    AMOUNT PAID: $" + payment);
     }
 
     //TODO 7 - Pay Tuition
@@ -66,7 +70,7 @@ public class Student {
         Scanner scanner = new Scanner(System.in);
         this.payment = scanner.nextInt();
         tuitionBalance = tuitionBalance - payment;
-        System.out.println("\nPAYMENT CONFIRMED: \nThanks for paying $" + payment + " of your total tuition");
+        System.out.println("\nPAYMENT CONFIRMED: \nCongratulations! You just paid your tuition");
         viewBalance();
     }
 
@@ -79,24 +83,42 @@ public class Student {
 
     //TODO 9 - Added feature/Options menu
     public void optionsMenu(){
-        int option;
-        do {
-            System.out.print("\nOPTIONS MENU\n1 - Enroll\n2 - Pay Tuition\n3 - View Balance\n4 - " +
-                    "Show Status (or Register Next Student)\nEnter an OPTION from the Menu:");
-            Scanner scanner = new Scanner(System.in);
-            option = scanner.nextInt();
-            if (!(option <1) & !(option >4)){
-                switch (option){
-                    case 1: enroll();
-                        break;
-                    case 2: payTuition();
-                        break;
-                    case 3: viewBalance();
-                        break;
-                    case 4: break;
-                    default: System.out.println("Invalid Entry");
+        try {
+            int option;
+            do {
+                System.out.print("\nOPTIONS MENU\n1 - Enroll\n2 - Pay Tuition\n3 - View Balance\n4 - " +
+                        "Show Status & QUIT(or Register Next Student)\nEnter an OPTION from the Menu:");
+                Scanner scanner = new Scanner(System.in);
+                option = scanner.nextInt();
+                if (!(option <1) & !(option >4)){
+                    switch (option){
+                        case 1: enroll();
+                            break;
+                        case 2: payTuition();
+                            break;
+                        case 3: viewBalance();
+                            break;
+                        case 4: break;
+                        default: System.out.println("   :::INVALID ENTRY:::");
+                    }
+                }else {
+                    System.out.println("\n  :::INVALID ENTRY:::");
                 }
-            }
-        }while (option!= 4);
+            }while (option!= 4);
+        }catch (InputMismatchException e){
+            System.out.println("    :::INVALID INPUT:::");
+            optionsMenu();
+        }
+    }
+    public int setGradeLevel(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("\nACADEMIC GRADE LEVELS\n1 - Junior\n2 - Intermediate\n3 - Senior\n4 - Advanced\nEnter " +
+                    "student's grade level: ");
+            this.gradeYear = scanner.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("\n  :::INVALID ENTRY:::");
+        }
+        return gradeYear;
     }
 }
